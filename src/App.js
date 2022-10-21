c7249107148553d0b77e43234191715b5e5261dc
maryam <ghorbanimoghaddam.m@gmail.com>
2022-10-21 18:05:11 +0300

    Change title, favico and fix antd css error

--- src/App.js
+++ src/App.js
@@ -1,8 +1,13 @@
+import { useEffect } from "react";
+
 import { HomePage } from "./pages/home/HomaPage";
 
 import "./App.css";
 
 const App = () => {
+  useEffect(() => {
+    document.title = "Solar Energy Production";
+  });
   return (
     <div className="App">
       <HomePage />
